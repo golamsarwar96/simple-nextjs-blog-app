133d2d9af53ed1f87343fc0c42f2fb6935169a34
Your Name <golamsarwar1896@gmail.com>
2025-01-23 17:26:30 +0600

    Added navbar and footer. Will work on design if have time

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,21 @@
+import { FaFacebook, FaFacebookF } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+
 const Footer = () => {
   return (
     <div>
-      <h1 className="text-4xl">Footer</h1>
+      <div className="container mx-auto flex flex-col md:flex-row justify-between items-center px-6 pt-3 pb-5 bg-gray-200">
+        <p className="text-3xl font-bold">Blog</p>
+        <div className="flex text-center flex-col md:flex-row justify-center items-center gap-3 md:text-3xl text-sm md:mt-0 mt-4">
+          <p className="text-lg">Copyright 2025, All Rights Reserved.</p>
+          <div className="flex gap-3 items-center justify-center text-2xl md:mt-0 mt-3">
+            <FaFacebookF />
+            <FaTwitter />
+            <FaLinkedin />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,27 @@
+import Link from "next/link";
+
 const Navbar = () => {
   return (
     <div>
-      <h1 className="text-4xl">Navbar</h1>
+      <nav className="container mx-auto flex justify-between items-center px-6 py-4 bg-gray-200">
+        <div>
+          <h1 className="text-4xl font-bold">Blog</h1>
+        </div>
+        <div className="flex justify-center items-center gap-4">
+          <Link href="/" prefetch={true}>
+            Home
+          </Link>
+          <Link href="/profile" prefetch={true}>
+            Profile
+          </Link>
+          <Link href="" prefetch={true}>
+            Login
+          </Link>
+          <Link href="" prefetch={true}>
+            Register
+          </Link>
+        </div>
+      </nav>
     </div>
   );
 };
